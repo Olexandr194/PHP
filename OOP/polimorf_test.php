d5e5eccace426e7e38b86aa6ada5835a2dfe3bc8
Olexandr194 <olexandrmirochnik@gmail.com>
2022-05-25 15:30:30 +0300

    add поліморфний виклик

--- OOP/polimorf_test.php
+++ OOP/polimorf_test.php
@@ -1,1 +1,29 @@
 <?php
+interface Machine {
+    public function calcTask();
+}
+class Circle implements Machine {
+    private $radius;
+    public function __construct($radius){
+        $this -> radius = $radius;
+    }
+    public function calcTask(){
+        return $this -> radius * $this -> radius * pi();
+    }
+}
+class Rectangle implements Machine {
+    private $width;
+    private $height;
+    public function __construct($width, $height){
+        $this -> width = $width;
+        $this -> height = $height;
+    }
+    public function calcTask(){
+        return $this -> width * $this -> height;
+    }
+}
+$a = new Circle(3);
+$b = new Rectangle(3,4);
+echo $a->calcTask();
+echo PHP_EOL;
+echo $b->calcTask();
