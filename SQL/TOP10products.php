a36b4f5982306de03af18531f02f5cab7288e983
Olexandr194 <olexandrmirochnik@gmail.com>
2022-07-13 22:35:10 +0300

    add SQL

---
+++ SQL/TOP10products.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'MySQL.php';
+
+$sql = new MySQL(array(
+    'host' => 'localhost',
+    'username' => 'root',
+    'passwords' => 'baohrkirkQEF/55',
+    'dbname' => 'laravel_shop',
+));
+
+$sql1 = "SELECT products.title as title, products.price as price
+         FROM products 
+         ORDER BY price DESC
+         LIMIT 10";
+
+$resultQuery = $sql->query($sql1);
+if ($resultQuery === false) {
+    throw new \Exception('Не вірний SQL запит!');
+}
+
+?>
+<h1>Вивід даних</h1>
+<table border="1">
+    <thead>
+    <th>Top 10 most expensive products</th><th>price</th>
+    </thead>
+    <tbody>
+    <?php foreach ($resultQuery as $row){ ?>
+        <tr>
+            <td><?=$row["title"]?></td><td><?=$row["price"]?></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+
+</table>
---
+++ SQL/bestCustomer.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'MySQL.php';
+
+$sql = new MySQL(array(
+    'host' => 'localhost',
+    'username' => 'root',
+    'passwords' => 'baohrkirkQEF/55',
+    'dbname' => 'laravel_shop',
+));
+
+$sql1 = "SELECT users.name as name, SUM(new_orders.product_qty) as qty
+	     FROM new_orders
+         INNER JOIN users ON new_orders.user_id = users.id
+         GROUP BY users.name
+            ORDER BY SUM(new_orders.product_qty) DESC
+            LIMIT 1";
+
+$resultQuery = $sql->query($sql1);
+if ($resultQuery === false) {
+    throw new \Exception('Не вірний SQL запит!');
+}
+
+?>
+<h1>Вивід даних</h1>
+<table border="1">
+    <thead>
+    <th>name</th><th>qty</th>
+    </thead>
+    <tbody>
+    <?php foreach ($resultQuery as $row){ ?>
+        <tr>
+            <td><?=$row["name"]?></td><td><?=$row["qty"]?>
+        </tr>
+    <?php } ?>
+    </tbody>
+
+</table>
+
